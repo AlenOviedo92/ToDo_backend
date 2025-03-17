a2c0fd3b2baa025941d35d675d4f77b8ae017630
Alen Oviedo <afoviedola@gmail.com>
2025-03-17 16:19:17 -0500

    Configuro CORS para deploy

--- src/app.js
+++ src/app.js
@@ -11,15 +11,30 @@ require('./db.js');
 
 const server = express();
 
+const allowedOrigins = FRONTEND_URL.split(',');
+
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors());                                                          
+server.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed for this origin'));
+    }
+  },
+  credentials: true
+}));                                                          
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', `${FRONTEND_URL}`);
+  // res.header('Access-Control-Allow-Origin', `${FRONTEND_URL}`);
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
